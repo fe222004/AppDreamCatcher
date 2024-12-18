e5aa9d871ee0fc74c50a9d1836c950bf9c84a42e
Fernanda Andrade <119636766+fe222004@users.noreply.github.com>
2024-12-18 08:26:32 -0500

    fondo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { NavModule } from "../components/nav/nav.module";
 import { NavComponent } from "../components/nav/nav.component";
+import { ImageConstants } from 'src/app/constants/images.constants';
 
 @Component({
   selector: 'app-home',  // El selector debe ser correcto
@@ -11,10 +12,14 @@ import { NavComponent } from "../components/nav/nav.component";
 export class HomeComponent {
   // Lógica del componente
   modalVisible = false;
+  public kitchenImage: String;
+
 
   constructor(private modalService: ModalService) {
     this.modalService.modalVisibility$.subscribe(visible => {
       this.modalVisible = visible;
     });
+        this.kitchenImage = ImageConstants.kitchen;
+    
   }
 }
